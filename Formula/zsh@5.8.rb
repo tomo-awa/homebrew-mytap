f1ea4389e05f7e166c778fcec13d96af59e3c174
tomo-awa <74037004+tomo-awa@users.noreply.github.com>
2022-10-10 04:23:27 +0000

    :sparkles: Add zsh another versions.
    On branch homebrew/zsh
    Changes to be committed:
    	new file:   Formula/zsh@5.4.rb
    	new file:   Formula/zsh@5.6.rb
    	new file:   Formula/zsh@5.7.rb
    	new file:   Formula/zsh@5.8.rb

---
+++ Formula/zsh@5.8.rb
@@ -0,0 +1,75 @@
+class ZshAT58 < Formula
+  desc "UNIX shell (command interpreter)"
+  homepage "https://www.zsh.org/"
+  url "https://downloads.sourceforge.net/project/zsh/zsh/5.8.1/zsh-5.8.1.tar.xz"
+  mirror "https://www.zsh.org/pub/zsh-5.8.1.tar.xz"
+  sha256 "b6973520bace600b4779200269b1e5d79e5f505ac4952058c11ad5bbf0dd9919"
+  license "MIT-Modern-Variant"
+
+  livecheck do
+    url "https://sourceforge.net/projects/zsh/rss?path=/zsh"
+  end
+
+  head do
+    url "https://git.code.sf.net/p/zsh/code.git", branch: "master"
+    depends_on "autoconf" => :build
+  end
+
+  depends_on "ncurses"
+  depends_on "pcre"
+
+  uses_from_macos "texinfo"
+
+  resource "htmldoc" do
+    url "https://downloads.sourceforge.net/project/zsh/zsh-doc/5.8.1/zsh-5.8.1-doc.tar.xz"
+    mirror "https://www.zsh.org/pub/zsh-5.8.1-doc.tar.xz"
+    sha256 "8b9cb53d6432f13e9767a8680b642e8e8a52c7f1b8decd211756ca20c667f917"
+  end
+
+  def install
+    # Work around configure issues with Xcode 12
+    # https://www.zsh.org/mla/workers/2020/index.html
+    # https://github.com/Homebrew/homebrew-core/issues/64921
+    ENV.append "CFLAGS", "-Wno-implicit-function-declaration"
+
+    system "Util/preconfig" if build.head?
+
+    system "./configure", "--prefix=#{prefix}",
+           "--enable-fndir=#{pkgshare}/functions",
+           "--enable-scriptdir=#{pkgshare}/scripts",
+           "--enable-site-fndir=#{HOMEBREW_PREFIX}/share/zsh/site-functions",
+           "--enable-site-scriptdir=#{HOMEBREW_PREFIX}/share/zsh/site-scripts",
+           "--enable-runhelpdir=#{pkgshare}/help",
+           "--enable-cap",
+           "--enable-maildir-support",
+           "--enable-multibyte",
+           "--enable-pcre",
+           "--enable-zsh-secure-free",
+           "--enable-unicode9",
+           "--enable-etcdir=/etc",
+           "--with-tcsetpgrp",
+           "DL_EXT=bundle"
+
+    # Do not version installation directories.
+    inreplace ["Makefile", "Src/Makefile"],
+              "$(libdir)/$(tzsh)/$(VERSION)", "$(libdir)"
+
+    if build.head?
+      # disable target install.man, because the required yodl comes neither with macOS nor Homebrew
+      # also disable install.runhelp and install.info because they would also fail or have no effect
+      system "make", "install.bin", "install.modules", "install.fns"
+    else
+      system "make", "install"
+      system "make", "install.info"
+
+      resource("htmldoc").stage do
+        (pkgshare/"htmldoc").install Dir["Doc/*.html"]
+      end
+    end
+  end
+
+  test do
+    assert_equal "homebrew", shell_output("#{bin}/zsh -c 'echo homebrew'").chomp
+    system bin/"zsh", "-c", "printf -v hello -- '%s'"
+  end
+end
